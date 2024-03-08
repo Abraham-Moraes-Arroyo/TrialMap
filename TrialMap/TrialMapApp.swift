e0b75e38e851827015251192c02d0d7b911361db
Abraham Morales Arroyo <abrahamof606@gmail.com>
2024-03-08 09:48:31 -0600

    Initial Commit

---
+++ TrialMap/TrialMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrialMapApp.swift
+//  TrialMap
+//
+//  Created by Abraham Morales Arroyo on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct TrialMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
